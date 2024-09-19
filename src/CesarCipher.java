2afa63e9c944d6f19d730cca9fd8f76df325bfb9
Солодовникова София Олеговна <217117@edu.fa.ru>
2024-09-19 13:42:57 +0300

    test

---
+++ src/CesarCipher.java
@@ -0,0 +1,19 @@
+public class CesarCipher {
+    String cipher(String message, int offset){
+        StringBuilder result = new StringBuilder();
+        for (char character : message.toCharArray()){
+            if (character !=' '){
+                int originalAlphabetPosition = character - 'a';
+                int newAlphabetPosition = (originalAlphabetPosition+offset) %26;
+                char newCharacter = (char) ('a'+newAlphabetPosition);
+                result.append(newCharacter);
+            }
+            else {
+                result.append(character);
+            }
+
+        }
+    }
+}
+
+
